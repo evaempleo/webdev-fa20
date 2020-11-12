50b9e8c716e0e98a964d51d80e520876579c7106
evaempleo <71097900+evaempleo@users.noreply.github.com>
2020-11-12 14:52:55 -0600

    Class

---
+++ class_code/jquery_color_toggle/main.js
@@ -0,0 +1,20 @@
+let wasClicked = false; 
+
+
+$('.box').click(function(){
+
+    if(wasClicked) {
+        $('.box').css("background", "lightgreen")
+    } else {
+        $('.box').css("background", "lightpink")
+    }
+
+
+    let newBox = ` <div class="box"> </div>` 
+
+    $('body').append(newBox);
+
+
+    wasClicked = !wasClicked; 
+})
+
--- class_code/jquery_intro/main.js
+++ class_code/jquery_intro/main.js
@@ -1,1 +1,21 @@
 console.log("hey")
+
+// Vanilla JavaScript
+let pageTitle = document.getElementById("page-title");
+pageTitle.style.color="red";
+
+// jQuery version
+
+$('#page-title-jquery').css("color", "blue");
+
+
+// EVENTS / EVENT LISTENERS 
+// $('.box').css("background", "red")
+$('.box').click(function(){
+    $('.box').css("background", "red")
+})
+
+$(window).on("wheel", function(){
+    $('.box').css("background", "red")
+})
+
