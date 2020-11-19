3040167095034efc251d85004e2b05dd0c8c4ea4
evaempleo <71097900+evaempleo@users.noreply.github.com>
2020-11-19 11:41:34 -0600

    modal works

--- class_portfolio/modal_popout/main.js
+++ class_portfolio/modal_popout/main.js
@@ -1,6 +1,14 @@
-
 $('.box').click(function(){
-    console.log("clicked!")
+    //console.log("clicked!")
     $(".box").addClass("animate__animated animate__shakeY");
 
+    $(".modal").show();
+
 });
+
+$('.modal').hide();
+
+$('.modal').click(function(){
+    $('.modal').hide();
+})
+
