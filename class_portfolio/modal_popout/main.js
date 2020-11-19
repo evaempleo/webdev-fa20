3522b4830ad79ac8261e851e465b5b62b798692a
evaempleo <71097900+evaempleo@users.noreply.github.com>
2020-11-18 23:19:13 -0600

    hjh

---
+++ class_portfolio/modal_popout/main.js
@@ -0,0 +1,11 @@
+$(".box").addClass("animated hinge");
+
+
+//  $("#target1").parent().css("background-color", "red");
+  //  $("#right-well").children().css("color", "orange");
+   /// $("#left-well").children().css("color", "green");
+   // $(".target:nth-child(2)").addClass("animated bounce");
+    //$(".target:even").addClass("animated shake");
+    // $("body").addClass("animated hinge");
+  
+
